92a143ecb4e1b489708b6875204e80403647eaf2
Graham Asam <grahamasam@gmail.com>
2025-03-07 19:08:28 -0500

    fetch map function and test button

--- map/backend/app.py
+++ map/backend/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+import json
+import os
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+CORS(app)  # Enable CORS to allow communication with the frontend
 
-@app.route('/api/ping')
-def ping():
-    return {'message': 'Pong'}
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MAP_FILE = os.path.join(BASE_DIR, "map.json")
+
+@app.route('/get_map', methods=['GET'])
+def get_map():
+  try:
+    # Read the JSON file
+    with open(MAP_FILE, 'r') as file:
+      data = json.load(file)
+
+    # Return the data as a JSON response
+    return jsonify(data), 200
+  except FileNotFoundError:
+    print("NO MAP")
+    return jsonify({"error": "Map file not found"}), 404
+  except json.JSONDecodeError:
+    return jsonify({"error": "Error decoding JSON"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- map/src/App.tsx
+++ map/src/App.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import MenuBar from "./Components/MenuBar";
+import axios from 'axios';
+
+type Map = number[][][];
 
 const App: React.FC = () => {
+  const [map, setMap] = useState<Map>([]);
+
+  const fetchMap = async (): Promise<void> => {
+    try {
+        const response = await axios.get("http://127.0.0.1:5000/get_map");
+        setMap(response.data.map);
+    } catch (error) {
+        console.error("Error fetching map:", error);
+    }
+  };
+
   return (
     <Router>
       <MenuBar />
+      <div>
+        <button onClick={fetchMap}>fetchMap</button>
+        {map} HAHA MAP
+      </div>
     </Router>
   );
 };
